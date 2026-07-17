d818dc6ed76bd51c4805c743258ed2d46b87668d
小金 <xiaojin@xiaojindeMacBook-Pro.local>
2026-07-17 11:57:58 +0800

    Use holiday Baibai on learning hub

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "learning-planet-v4";
-const FILES = ["./", "./index.html", "./app.js", "./manifest.json", "./assets/baibai-base.png"];
+const CACHE = "learning-planet-v5";
+const FILES = ["./", "./index.html", "./app.js", "./manifest.json", "./assets/baibai-holiday-cutout.webp"];
 self.addEventListener("install", e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting())));
 self.addEventListener("activate", e => e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())));
 self.addEventListener("fetch", e => {
